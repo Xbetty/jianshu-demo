7882a282657f7c54f0a13fcf4dd87e371ccd4e57
Xbetty <betty.xiong@pdabc.com>
2019-08-23 18:32:15 +0800

    redux-immutable

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux-immutable';
 // ES6语法 为防止重名，给reducer起别名headerReducer
 import { reducer as headerReducer } from '../common/header/store';
 
-
+// 生成immutable对象
 const reducer = combineReducers({
     header: headerReducer
 });
